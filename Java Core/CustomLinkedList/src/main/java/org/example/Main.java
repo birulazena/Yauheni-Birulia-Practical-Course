1534326e1e06b556ed44cd4baf6df19d7a154bf1
birulya.zhenka <birulya.zhenka@gmail.com>
2025-10-07 19:02:46 +0300

    готовый лист с тестами

---
+++ Java Core/CustomLinkedList/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        MyList<Integer> check = new MyList<Integer>();
+//        check.addFirst(12);
+//        check.addFirst(13);
+//        check.addFirst(14);
+//
+//        check.addLast(11);
+//        check.addLast(10);
+//        check.addLast(9);
+//
+//        check.add(0, 52);
+//        System.out.println(check.get(0));
+//        System.out.println(check.get(1));
+//        System.out.println(check.get(2));
+//        System.out.println(check.get(3));
+//        System.out.println(check.get(4));
+//        System.out.println(check.get(5));
+//        System.out.println(check.get(6));
+
+//        check.addLast(12);
+//        check.addFirst(13);
+//        check.addFirst(14);
+//        check.addFirst(15);
+//        check.remote(2);
+
+        check.add(0, 11);
+        check.add(1, 22);
+        check.add(2, 33);
+
+
+
+    }
+}
