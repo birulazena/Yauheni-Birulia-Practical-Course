4035d975cd1d7f2794daeeeb9add630bc5b7e1e4
birulya.zhenka <birulya.zhenka@gmail.com>
2025-10-07 19:18:27 +0300

    добавил классы из задания

---
+++ Java Core/SalesAndCustomerAnalysis/src/main/java/org/example/Order.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Order {
+    private String orderId;
+    private LocalDateTime orderDate;
+    private Customer customer;
+    private List<OrderItem> items;
+    private OrderStatus status;
+}
---
+++ Java Core/SalesAndCustomerAnalysis/src/main/java/org/example/OrderItem.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class OrderItem {
+    private String productName;
+    private int quantity;
+    private double price;
+    private Category category;
+}
