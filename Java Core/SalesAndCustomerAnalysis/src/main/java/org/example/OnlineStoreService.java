d18f1aa208eb592a5261b8f49d0b359edfbf9d8d
birulya.zhenka <birulya.zhenka@gmail.com>
2025-10-08 21:40:38 +0300

    добавил третий метод mostPopularProductBySales

--- Java Core/SalesAndCustomerAnalysis/src/main/java/org/example/OnlineStoreService.java
+++ Java Core/SalesAndCustomerAnalysis/src/main/java/org/example/OnlineStoreService.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class OnlineStoreService {
 
@@ -25,6 +29,18 @@ public class OnlineStoreService {
                 .mapToDouble(c -> c.getItems().stream().mapToDouble(i -> i.getPrice()).sum())
                 .sum();
 
+    }
+
+    public String mostPopularProductBySales() {
+        return orders.stream()
+                .flatMap(i -> i.getItems()
+                        .stream())
+                .collect(Collectors.groupingBy(OrderItem::getProductName, Collectors.summingInt(OrderItem::getQuantity)))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .toString();
     }
 
 }
