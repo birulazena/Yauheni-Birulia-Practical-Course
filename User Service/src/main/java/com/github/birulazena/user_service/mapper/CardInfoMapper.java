29ea28c60aad9057e09ccbb6bbbaddf68c5696d4
birulya.zhenka <birulya.zhenka@gmail.com>
2025-10-20 14:32:45 +0300

    добавил ошибку, маппер, сервис, и метод для маппинг карт

--- User Service/src/main/java/com/github/birulazena/user_service/mapper/CardInfoMapper.java
+++ User Service/src/main/java/com/github/birulazena/user_service/mapper/CardInfoMapper.java
@@ -3,27 +3,25 @@ package com.github.birulazena.user_service.mapper;
 import com.github.birulazena.user_service.dto.CardInfoRequestDto;
 import com.github.birulazena.user_service.dto.CardInfoResponseDto;
 import com.github.birulazena.user_service.entities.CardInfo;
+import com.github.birulazena.user_service.utils.CardInfoUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
-        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        imports = CardInfoUtils.class)
 public interface CardInfoMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "user", ignore = true)
     CardInfo toEntity(CardInfoRequestDto cardInfoRequestDto);
 
-    @Mapper()
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "hiddenNumber", expression = "java(CardInfoUtils.hideTheNumber(cardInfo.getNumber()))")
     CardInfoResponseDto toDto(CardInfo cardInfo);
 
-    private String hideTheNumber(String number) {
-        if (number == null || number.length() <= 4) {
-            return number; // или вернуть "****"
-        }
-        return "*".repeat(number.length() - 4) + number.substring(number.length() - 4);
-    }
+
 
 }
--- User Service/src/main/java/com/github/birulazena/user_service/service/UserService.java
+++ User Service/src/main/java/com/github/birulazena/user_service/service/UserService.java
@@ -1,10 +1,14 @@
 package com.github.birulazena.user_service.service;
 
+import com.github.birulazena.user_service.entities.User;
+import com.github.birulazena.user_service.exception.UserNotFoundException;
 import com.github.birulazena.user_service.repository.CardInfoRepository;
 import com.github.birulazena.user_service.repository.UserRepositoryJpa;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @AllArgsConstructor
@@ -13,8 +17,21 @@ public class UserService {
     private CardInfoRepository cardInfoRepository;
     private UserRepositoryJpa userRepositoryJpa;
 
+    public User createUser(User user) {
+        return userRepositoryJpa.save(user);
+    }
 
+    public User getUserById(Long id) {
+        return userRepositoryJpa.findById(id).orElseThrow(() -> new UserNotFoundException("User not found ID: " + id));
+    }
 
+    public Page<User> findAll(Pageable pageable) {
+        return userRepositoryJpa.findAll(pageable);
+    }
+
+    public User getUserByEmail(String email) {
+        userRepositoryJpa.
+    }
 
 
 }
