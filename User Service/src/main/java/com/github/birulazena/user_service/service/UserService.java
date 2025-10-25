4e5db4387dda7055ae28887faf8bdc4089cb3d5a
birulya.zhenka <birulya.zhenka@gmail.com>
2025-10-25 17:07:40 +0300

    Готовое задание Caching

--- User Service/src/main/java/com/github/birulazena/user_service/service/UserService.java
+++ User Service/src/main/java/com/github/birulazena/user_service/service/UserService.java
@@ -14,6 +14,11 @@ import com.github.birulazena.user_service.mapper.UserMapper;
 import com.github.birulazena.user_service.repository.CardInfoRepository;
 import com.github.birulazena.user_service.repository.UserRepositoryJpa;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -29,7 +34,14 @@ public class UserService {
     private UserRepositoryJpa userRepositoryJpa;
     private UserMapper userMapper;
     private CardInfoMapper cardInfoMapper;
+    private CacheManager cacheManager;
 
+    @Caching(
+            put = {
+                    @CachePut(value = "user_cache", key = "#result.id()"),
+                    @CachePut(value = "user_email_cache", key = "#result.email()")
+            }
+    )
     public UserResponseDto createUser(UserRequestDto userDto) {
 
         if(userRepositoryJpa.findByEmail(userDto.email()).isPresent()) {
@@ -41,6 +53,7 @@ public class UserService {
         return userMapper.toDto(saveUser);
     }
 
+    @Cacheable(value = "user_cache", key = "#id")
     public UserResponseDto getUserById(Long id) {
         User user = userRepositoryJpa.findById(id).orElseThrow(() -> new UserNotFoundException("User not found ID: " + id));
         return userMapper.toDto(user);
@@ -51,12 +64,19 @@ public class UserService {
         return userRepositoryJpa.findAll(pageable).map(u -> userMapper.toDto(u));
     }
 
+    @Cacheable(value = "user_email_cache", key = "#email")
     public UserResponseDto getUserByEmail(String email) {
         User user = userRepositoryJpa.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found email: " + email));
         return userMapper.toDto(user);
     }
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
+    @Caching(
+            put = {
+                    @CachePut(value = "user_cache", key = "#id"),
+                    @CachePut(value = "user_email_cache", key = "#result.email()")
+            }
+    )
     public UserResponseDto updateUser(Long id, UserRequestDto userDto) {
         userRepositoryJpa.findById(id).orElseThrow(() -> new UserNotFoundException("User not found ID: " + id));
         User user = userMapper.toEntity(userDto);
@@ -65,11 +85,14 @@ public class UserService {
         return userMapper.toDto(saveUser);
     }
 
-    @Transactional(isolation = Isolation.READ_COMMITTED)
+
+    @CacheEvict(value = "user_cache", key = "#id")
     public void deleteUserById(Long id) {
-        if(userRepositoryJpa.findById(id).isEmpty()) {
-            throw new UserNotFoundException("User not fount ID: " + id);
-        }
+        User user = userRepositoryJpa.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not fount ID: " + id));
+
+        cacheManager.getCache("user_email_cache").evict(user.getEmail());
+
         userRepositoryJpa.deleteById(id);
     }
 
