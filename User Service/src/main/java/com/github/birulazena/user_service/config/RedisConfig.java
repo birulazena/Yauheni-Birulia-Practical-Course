28c2e20e75b6b69061d3a9d7fd83ff76cb6a1fb5
birulya.zhenka <birulya.zhenka@gmail.com>
2025-10-30 23:10:25 +0300

    немного подправил проект в ветке докер, и добавил тут, чтобы была актуальная версия

--- User Service/src/main/java/com/github/birulazena/user_service/config/RedisConfig.java
+++ User Service/src/main/java/com/github/birulazena/user_service/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.birulazena.user_service.dto.UserResponseDto;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.serializer.*;
 import java.time.Duration;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Bean
